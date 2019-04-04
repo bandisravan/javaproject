b5656da395701068a32ae46f931a3ce3d175cd40
PanaromicHCL <>
2019-04-04 15:14:17 +0530

    committing

---
+++ src/main/java/com/example/controller/DPBController.java
@@ -0,0 +1,37 @@
+package com.example.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import org.springframework.http.HttpStatus;
+
+@RestController
+@RequestMapping("/api")
+public class DPBController extends SpringBootServletInitializer{
+	
+	
+	
+	@RequestMapping(value="/hello",method=RequestMethod.GET)
+	public Boolean addUser() {
+		return true;
+	}
+	
+}
